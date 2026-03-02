cfd7cab124e06e517216dc061a6c3ba6bee9f115
David O <doluwamakinde@gmail.com>
2026-03-01 19:47:04 -0600

    Update product_order.sql

--- database/product_order.sql
+++ database/product_order.sql
@@ -3,6 +3,6 @@ quantity SMALLINT,
 productID INT,
 transactionID INT,
 PRIMARY KEY (transactionID, productID),
-FOREIGN KEY (transactionID) REFERENCES placeholder_table(placeholder_column),
-FOREIGN KEY (productID) REFERENCES placeholder_table(placeholder_column)
+FOREIGN KEY (transactionID) REFERENCES transactions(transactionID),
+FOREIGN KEY (productID) REFERENCES product(productID)
 );
