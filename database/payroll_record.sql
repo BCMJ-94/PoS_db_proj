53a07c031fe49747c57dcda36a317a767fb995ad
Aaron Lopez <aarlopez5@gmail.com>
2026-03-01 20:24:58 -0600

    Changed variable names

--- database/payroll_record.sql
+++ database/payroll_record.sql
@@ -1,10 +1,10 @@
 CREATE TABLE payroll_records(
-    emloyeeID INT,
+    employeeID INT,
     payPeriodID INT,
     totalHours FLOAT,
     totalPay FLOAT,
 
     PRIMARY KEY (employeeID, payPeriodID),
-    -- FOREIGN KEY (employeeID) REFERENCES EMPLOYEES (employeeID)
-    FOREIGN KEY (payPeriodID) REFERENCES PAY_PERIOD (payPeriodID)
+    FOREIGN KEY (employeeID) REFERENCES employees (employeeID),
+    FOREIGN KEY (payPeriodID) REFERENCES pay_periods (payPeriodID)
 );
