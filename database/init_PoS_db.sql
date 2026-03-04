c14699e2fdbc6c6856ed9514ad06116f18061c31
BCMJ-94 <bcmooney@cougarnet.uh.edu>
2026-03-04 15:17:13 -0600

    Update init_PoS_db.sql
    added constraints, changed some attr types

--- database/init_PoS_db.sql
+++ database/init_PoS_db.sql
@@ -2,18 +2,22 @@ CREATE DATABASE RestaurantTestDB;
 
 CREATE TABLE ingredients(
 	ingredientID INT PRIMARY KEY,
-    _name SMALLINT,
-    pricePerUnit FLOAT,
+    _name VARCHAR(15),
+    pricePerUnit FLOAT NOT NULL,
     quantity FLOAT
+
+    CONSTRAINT priceCheck CHECK (pricePerUnit > 0)
 );
 
 CREATE TABLE products(
 	productID INT PRIMARY KEY,
-    _name SMALLINT,
-    price FLOAT,
+    _name SMALLINT NOT NULL,
+    price FLOAT NOT NULL,
     menuType SMALLINT,
-    isAvailable BOOL,
+    isAvailable BOOL NOT NULL,
     stationID SMALLINT
+
+    CONSTRAINT priceCheck CHECK (price > 0) 
 );
 
 CREATE TABLE transactions(
@@ -21,14 +25,14 @@ CREATE TABLE transactions(
     tableID INT,
     employeeID INT NOT NULL,
     customerID INT,
-    timePlaced DATETIME,
+    timePlaced DATETIME NOT NULL,
     total FLOAT,
     tipAmount FLOAT,
     paymentMethod SMALLINT
 );
 
 CREATE TABLE purchase_orders(
-	orderID INT,
+	orderID INT NOT NULL,
     supplierName VARCHAR(50),
     ingredientID INT,
     quantity INT,
