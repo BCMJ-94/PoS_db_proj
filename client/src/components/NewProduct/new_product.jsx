bebb67ca93c02ba96b42daaab73bc67aa56e1fb6
BCMJ-94 <bcmooney@cougarnet.uh.edu>
2026-03-18 00:04:59 -0500

    modified:   client/src/components/NewEmployee/NewEmployee.jsx 	new file:   client/src/components/NewEmployee/NewProduct.jsx 	modified:   client/src/components/NewEmployee/new_employee_form.jsx 	new file:   client/src/components/NewIngredient/NewIngredient.jsx 	deleted:    client/src/components/NewIngredient/new_ingredient.jsx 	new file:   client/src/components/NewIngredient/new_ingredient_form.jsx 	modified:   client/src/components/NewProduct/new_product.jsx

--- client/src/components/NewProduct/new_product.jsx
+++ client/src/components/NewProduct/new_product.jsx
@@ -1,36 +1,43 @@
+import React from 'react'
+import Button from '../Button.jsx'
 
-function NewProduct(){
+function NewProductForm({handleSubmit}){
 
 
-    const handleClick = () =>{};
+    const onSubmit= (e) =>{
+        e.preventDefault()
+        const form = new FormData()
+        handleSubmit({productID : form.get('productID'),
+                      _name : form.get('_name'),
+                      price : form.get('price'),
+                      menuType : form.get('menuType'),
+                      stationID : form.get('stationID')
+        })
+    };
+
     return(
-        <>
-        <form>
-            <header>Add New Product</header>
-            <label>
-                ProductID 
-                <input name = "productID"/> <br/>
-            </label>
-            <label>
-                Name 
-                <input name = "_name"/> <br/>
-            </label>
-            <label>
-                Price
-                <input name = "price"/> <br/>
-            </label>
-            <label>
-                Menu Type 
-                <input name = "menuType"/> <br/>
-            </label>
-            <label>
-                StationID 
-                <input name = "stationID"/> <br/>
-            </label>
+        <div>
+            <form method = "post" onSubmit = {onSubmit} className = 'flex flex-col items-center gap-5'>
+                <div className = 'flex flex-col gap-1'>
+                    <label htmlFor = "productID"> ProductID </label>
+                    <input type = "text" name = "productID" id = "productID" required className = 'border border-[#7ebeeba6] rounded-md px-2'/>
+                </div>
 
-            <button onClick = {handleClick}> Submit </button>
-        </form>
-        </>
+                <div className = 'flex flex-col gap-1'>
+                    <label htmlFor = "_name"> Name </label>
+                    <input type = "text" name = "_name" id = "_name" required className = 'border border-[#7ebeeba6] rounded-md px-2'/>
+                </div>
+
+                <div className = 'flex flex-col gap-1'>
+                    <label htmlFor = "menuType"> Menu Type </label>
+                    <input type = "text" name = "menuType" id = "menuType" required className = 'border border-[#7ebeeba6] rounded-md px-2'/>
+                </div>
+                <div className = 'flex flex-col gap-1'>
+                    <label htmlFor = "stationID"> Station ID </label>
+                    <input type = "text" name = "stationID" id = "stationID" required className = 'border border-[#7ebeeba6] rounded-md px-2'/>
+                </div>
+            </form>
+        </div>
     );
 }
 
