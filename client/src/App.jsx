1bd9557d6727368c93de497eff50e6c6a35d8036
BCMJ-94 <bcmooney@cougarnet.uh.edu>
2026-03-19 16:18:49 -0500

    modified:   client/src/App.jsx 	deleted:    package-lock.json 	deleted:    package.json

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -9,7 +9,8 @@ function App(){
 	return (
         <>
             {view === "login" ? (
-                <Apitest/>
+                <Login/>
+
             ) :
             null}
             <button 
