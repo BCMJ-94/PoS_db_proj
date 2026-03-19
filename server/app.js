a093a09c5633bd8d6326ef9223e72ef5f1868fa7
BCMJ-94 <bcmooney@cougarnet.uh.edu>
2026-03-18 22:57:20 -0500

    fixed an accidental commit, my bad

--- server/app.js
+++ server/app.js
@@ -39,38 +39,3 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
     console.log(`Listening on port ${port}`)
 })
-import express from 'express'
-import { getEmployees, getEmployee, createEmployee } from './database.js'
-
-const app = express()
-
-app.use(express.json())
-
-app.get("/employees", async (req, res) => { // creates a route /employees on the webapp that displays the list of employees
-    const employees = await getEmployees() // make sure async is present so that await works
-    res.send(employees)
-
-})
-
-app.get("/employee/:employeeID", async (req, res) => { // creates a route /employees/id that shows a given employee
-    const employeeID = req.params.employeeID
-    const employee = await getEmployee(employeeID)
-    res.send(employee)
-})
-
-app.post("/employees", async (req, res) => { // creates a new employee using the createEmployee function from database.js and adds it to the /employees path (where the list of employees are)
-    const {firstName, lastName, dateHired, dateOfBirth, ShiftRole, hourlyRate} = req.body
-    const employee = await createEmployee(firstName, lastName, dateHired, dateOfBirth, ShiftRole, hourlyRate)
-    //res.status(201).send(note)
-})
-
-// error handling?
-
-app.get('/user/:id', async (req, res, next) => {
-  const user = await getUserById(req.params.id)
-  res.send(user)
-})
-
-app.listen(8080, () => {
-    console.log("Server is running on port 8080")
-})
