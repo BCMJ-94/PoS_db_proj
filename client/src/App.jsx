5734b6c957e4d7460e3d9e2ace90c816c3f4db76
Aaron Lopez <aarlopez5@gmail.com>
2026-03-20 07:33:25 -0500

    Removed useState that was not needed in app

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,6 @@
-import { useState } from "react";
 import AppRoutes from "./routes/AppRoutes";
 
 function App(){
-    const [view, setView] = useState("login")
-
 	return (
         <AppRoutes />
     )
