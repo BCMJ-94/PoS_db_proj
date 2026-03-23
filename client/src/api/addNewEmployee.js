4e3a743ab28ee9578ad3e3b0495e9ed882d06ceb
BCMJ-94 <bcmooney@cougarnet.uh.edu>
2026-03-23 12:26:40 -0500

    Add files via upload

---
+++ client/src/api/addNewEmployee.js
@@ -0,0 +1,28 @@
+
+export default async function addNewEmployee({firstName, lastName, dateHired, 
+    dateOfBirth, shiftRole, hourlyRate, password}){
+    const endpoint = 'http://localhost:3030/employees'
+
+    try {
+        const response = await fetch(endpoint, {
+            method : 'POST',
+            headers: {"Content-Type" : "application/json"},
+            credentials : "include",
+            body : JSON.stringify({firstName, lastName, dateHired, dateOfBirth, shiftRole, hourlyRate, password})
+        })
+        if (!response.ok){
+            const err = await response.json().catch() (() => ({}));
+            throw new Error (
+                err.message || `HTTP Error: ${response.status}`
+            );
+        }
+        const data = await response.text()
+        console.log(data)
+    }
+
+    catch(error){
+        console.error('fetch error: ', error)
+        throw error;
+    }
+
+}
