9452fc70280c3616570cc5f25300b03350e6235d
nileink <aincekara40@gmail.com>
2026-03-24 18:51:47 -0500

    added delete and update functions to the routes

--- server/routes/sectionsRoute.js
+++ server/routes/sectionsRoute.js
@@ -1,6 +1,6 @@
 import express from 'express'
 
-import { getSection, getSections, createSection } from '../database.js'
+import { getSection, getSections, createSection, updateSection, deleteSection} from '../database.js'
 import isAuthorized from '../utils/auth.js'
 
 const sectionsRouter = express.Router()
@@ -43,6 +43,49 @@ sectionsRouter.post("/", async (req, res) => {
     try {
         const section = await createSection(employeeID)
         res.status(201).send(section)
+    } catch (err) {
+        res.status(500).json({
+            message: "Server error"
+        })
+    }
+})
+
+sectionsRouter.put("/:sectionID", async (req, res) => {
+    const sectionID = req.params.sectionID
+
+    try {
+        const { employeeID } = req.body
+        const section = await getSections(sectionID)
+
+        if (!section) {
+            return res.status(404).json({
+                message: "Section not found"
+            })
+        }
+
+        const updatedSection = await updateSection(employeeID, sectionID)
+        res.send(updatedSection)
+    } catch (err) {
+        res.status(500).json({
+            message: "Server error"
+        })
+    }
+})
+
+sectionsRouter.delete("/:sectionID", async (req, res) => {
+    const sectionID = req.params.sectionID
+
+    try {
+        const section = await getSections(sectionID)
+
+        if (!section) {
+            return res.status(404).json({
+                message: "Section not found"
+            })
+        }
+
+        await deleteSection(sectionID)
+        res.sendStatus(204)
     } catch (err) {
         res.status(500).json({
             message: "Server error"
