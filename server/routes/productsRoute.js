5b15a531fbafe61f98664412814754aaafdcb9c8
nileink <aincekara40@gmail.com>
2026-03-24 21:40:59 -0500

    added delete routes

--- server/routes/productsRoute.js
+++ server/routes/productsRoute.js
@@ -1,6 +1,6 @@
 import express from 'express'
 
-import { getProduct, getProducts, createProduct } from '../database.js'
+import { getProduct, getProducts, createProduct, updateProduct, deleteProduct } from '../database.js'
 import isAuthorized from '../utils/auth.js'
 
 const productsRouter = express.Router()
@@ -24,7 +24,7 @@ productsRouter.get("/:productID", async (req, res) => {
         const product = await getProduct(productID)
 
         if (!product) {
-            res.status(404).json({
+            return res.status(404).json({
                 message: "Product not found"
             })
         }
@@ -43,6 +43,32 @@ productsRouter.post("/", async (req, res) => {
     try {
         const product = await createProduct(productID, _name, price, menuType, isAvailable, stationID)
         res.status(201).send(product)
+    } catch (err) {
+        res.status(500).json({
+            message: "Server error"
+        })
+    }
+})
+
+productsRouter.put("/", async (req, res) => {
+    const { _name, price, menuType, isAvailable, stationID, productID } = req.body
+
+    try {
+        const product = await updateProduct(_name, price, menuType, isAvailable, stationID, productID)
+        res.status(200).send(product)
+    } catch (err) {
+        res.status(500).json({
+            message: "Server error"
+        })
+    }
+})
+
+productsRouter.delete("/", async (req, res) => {
+    const { productID, _name } = req.body
+
+    try {
+        const product = await deleteProduct(productID, _name)
+        res.sendStatus(204)
     } catch (err) {
         res.status(500).json({
             message: "Server error"
