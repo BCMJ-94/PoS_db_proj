839163ac718941c9c794114f093ab6a5705c2cfe
David O <doluwamakinde@gmail.com>
2026-04-20 10:48:29 -0500

    ok

--- server/encrypt_password.js
+++ server/encrypt_password.js
@@ -4,14 +4,9 @@ import dotenv from 'dotenv'
 import mysql from 'mysql2'
 
 dotenv.config()
-const pool = mysql.createPool({
-    host: process.env.MYSQL_HOST,
-    user: process.env.MYSQL_USER,
-    password: process.env.MYSQL_PASSWORD,
-    database: process.env.MYSQL_DATABASE
-}).promise()
+
 
 const password = "testpass"
 const saltRounds = 10
 const hashedPassword = await hash(password, saltRounds)
-const result = await createEmployee("first", "last", "1994-01-01", "1994-01-01", "1", "30", hashedPassword)
+const result = await createEmployee("first", "last", "2026-01-01", "1994-01-01", "1", "30", hashedPassword)
